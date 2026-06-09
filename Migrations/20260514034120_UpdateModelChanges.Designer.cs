92a8a983de15b5a89c4616fc7c7e5dd5bb99372f
marwabe-dev <belahcenemarwa62@gmail.com>
2026-06-09 15:38:14 +0100

    Add files via upload

--- Migrations/20260514034120_UpdateModelChanges.Designer.cs
+++ Migrations/20260514034120_UpdateModelChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EduPsych_Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EduPsych_Web.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260514034120_UpdateModelChanges")]
+    partial class UpdateModelChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260514034120_UpdateModelChanges.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduPsych_Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "counselor_id1",
+                table: "withdrawal_requests");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "amount",
+                table: "withdrawal_requests",
+                type: "numeric(12,2)",
+                precision: 12,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "amount",
+                table: "withdrawal_requests",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12,2)",
+                oldPrecision: 12,
+                oldScale: 2);
+
+            migrationBuilder.AddColumn<long>(
+                name: "counselor_id1",
+                table: "withdrawal_requests",
+                type: "bigint",
+                nullable: true);
+        }
+    }
+}
